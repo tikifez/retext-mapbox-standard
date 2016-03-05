f417e78b9023cbbe469cb6996a80e349705f0728
Xristopher Anderton <xris@tikidream.com>
2016-03-05 12:45:32 -0500

    Added brands for testing

--- data/brands.js
+++ data/brands.js
@@ -3,5 +3,7 @@ module.exports = {
     'Open Street Map': { replace: ['OpenStreetMap'] },
     'Open Street Maps': { replace: ['OpenStreetMap'] },
     TIF: { replace: ['TIFF'] },
-    MapBox: { cased: true, replace: ['Mapbox'] }
+    MapBox: { cased: true, replace: ['Mapbox'] },
+    JPG: { replace: ['JPG']},
+    inkfish: { replace: ['Inkfish']}
 };
